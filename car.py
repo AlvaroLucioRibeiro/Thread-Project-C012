60f973c871c74f07dbc16f1e7b4f73ed18282e86
AlvaroLucioRibeiro <alvaroluciorib@gmail.com>
2024-03-27 19:55:46 -0300

    Update Codes

--- car.py
+++ car.py
@@ -4,27 +4,48 @@ from threading import Thread
 from tqdm import tqdm
 
 class Car(Thread):
-  winners = {}
-  position = 0
+    """Uma classe Car que estende Thread, simulando uma corrida de carros onde cada carro corre em sua própria thread.
+    
+    Atributos:
+        winners (dict): Uma variável de classe que armazena os vencedores da corrida em ordem.
+        position (int): Uma variável de classe que rastreia a posição de chegada dos carros.
+    """
+    
+    winners = {}
+    position = 0
 
-  def __init__(self, name, speed, color):
-    super().__init__()
-    self.name = name
-    self.speed = speed
-    self.color = color
-    self.displacement = 0
-
-  def run(self):
-    with tqdm(total=100, desc=self.name, colour=self.color, leave=False, bar_format='{desc:<20}: {bar} {n_fmt}/{total_fmt}', ncols=100) as progress_bar:
-        while self.displacement < 100:
-            self.displacement += self.speed
-            progress_bar.update(self.speed)
-
-            if randint(1, 10) == 7:
-                sleep(6)
-
-            sleep(2)
-
-        Car.position += 1
-        Car.winners[self.name] = Car.position
+    def __init__(self, name, speed, color):
+        """Inicializa um novo objeto Car.
         
+        Args:
+            name (str): O nome do carro.
+            speed (int): A velocidade com que o carro se move (distância por unidade de tempo).
+            color (str): A cor da barra de progresso no console.
+        """
+        super().__init__()
+        self.name = name
+        self.speed = speed
+        self.color = color
+        self.displacement = 0
+
+    def run(self):
+        """Simula a corrida do carro.
+        
+        Usa uma barra de progresso para exibir visualmente a corrida no console. O progresso do carro é atualizado
+        com base em sua velocidade. Aleatoriamente, o carro pode 'dormir' por 6 segundos para simular um atraso. Uma vez que o carro
+        termina a corrida, seu nome e posição são registrados na variável de classe 'winners'.
+        """
+        with tqdm(total=100, desc=self.name, colour=self.color, leave=False, 
+                  bar_format='{desc:<20}: {bar} {n_fmt}/{total_fmt}', ncols=100) as progress_bar:
+            while self.displacement < 100:
+                self.displacement += self.speed
+                progress_bar.update(self.speed)
+
+                # Simula aleatoriamente um atraso na corrida
+                if randint(1, 10) == 7:
+                    sleep(6)
+
+                sleep(2)  # Simula a passagem do tempo entre os movimentos
+
+            Car.position += 1
+            Car.winners[self.name] = Car.position
--- main.py
+++ main.py
@@ -3,33 +3,41 @@ from car import Car
 from random import randint
 import os
 
+# Lista de nomes dos participantes da corrida
 persons = ["Barão Vermelho", "Penelope", "Irmãos Rocha", "Dick Vigarista", "Dupla Sinistra", "Professor Aéreo", "Tomas e Urso", "Peter Perfeito", "Meekley", "Quadrilha da Morte"]
-colors = ["#FF7F50", "#48D1CC", "#DB7093", "#1E90FF", "#D2691E", "#87CEEB", "#DAA520", "#98FB98", "#3CB371", "#008B8B"]
-cars = []
 
-color = 0
+# Lista de cores associadas a cada participante para uso na interface gráfica da corrida
+colors = ["#FF7F50", "#48D1CC", "#DB7093", "#1E90FF", "#D2691E", "#87CEEB", "#DAA520", "#98FB98", "#3CB371", "#008B8B"]
+cars = []  # Lista para armazenar os objetos Car criados para cada participante
+
+color = 0  # Índice para acessar a lista de cores
+
 for person in persons:
-  speed = randint(5, 10)
-  cars.append(Car(person, speed, colors[color]))
-  color += 1
+  speed = randint(5, 10)  # Gera uma velocidade aleatória para cada carro entre 5 e 10
+  cars.append(Car(person, speed, colors[color]))  # Cria um objeto Car e o adiciona à lista de carros
+  color += 1  # Incrementa o índice de cores para usar a próxima cor disponível
 
 print("\n############ A CORRIDA VAI COMEÇAR ############\n")
 print("-----------CORREDORES-----------")
 
+# Exibe os nomes e velocidades dos carros participantes
 for car in cars:
   print(f"{car.name} -> velocidade: {car.speed}")
 
-sleep(5)
-os.system('cls')
+sleep(5)  # Aguarda 5 segundos antes de começar a corrida
+os.system("cls")  # Limpa a tela do terminal para iniciar a corrida
   
+# Inicia a thread de cada carro, começando a corrida
 for car in cars:
   car.start()
 
+# Aguarda a finalização de todas as threads dos carros
 for car in cars:
   car.join()
 
-os.system('cls')
+os.system('cls')  # Limpa a tela do terminal para mostrar os resultados
 
 print("-----------RESULTADO-----------")
+# Exibe o resultado da corrida com a posição e nome de cada carro
 for c in Car.winners:
   print(f"{Car.winners[c]}º lugar: {c}")
